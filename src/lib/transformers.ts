a4078fa562c4ecab04e1e8dc533ff7327977b433
David Hunt <git@dnotes.net>
2021-11-21 15:43:11 -0800

    added most basic html sanitizer

--- src/lib/transformers.ts
+++ src/lib/transformers.ts
@@ -1,13 +1,42 @@
 import type { SvelteCMSFieldTransformer } from './global'
+import sanitizeHtml from 'sanitize-html'
 
 const transformers:{[id:string]:SvelteCMSFieldTransformer} = {
   toString: {
     id: 'toString',
-    fn: (v) => v.toString()
+    fn: (v) => v?.toString()
   },
   date: {
     id: 'date',
     fn: (v) => new Date(v)
+  },
+  html: {
+    id: 'html',
+    fn: (v) => { return sanitizeHtml(v) }
+  },
+  dateFormat: {
+    id: 'dateFormat',
+    fn: (v,opts) => {
+      v = new Date(v)
+      return v.toLocaleDateString(opts.locale, opts.dateFormatOptions)
+    },
+    optionFields: {
+      locale: {
+        type: 'text',
+        default: '',
+        description: "The locale to display the date, e.g. 'en-US', or empty to display in client's locale."
+      },
+      dateFormatOptions: {
+        type: 'collection',
+        default: {},
+        fields: {
+          timeZone: {
+            type: 'text',
+            default: '',
+          },
+        }
+      }
+    }
   },
   boolean: {
     id: 'boolean',
@@ -25,7 +54,7 @@ const transformers:{[id:string]:SvelteCMSFieldTransformer} = {
     id: 'tags',
     fn: function parseTags(v,opts) {
       let regex = opts.allowSpaces ? new RegExp(`\s*${opts.delimiter}\s*`,'g') : new RegExp(`(?:\s|${opts.delimiter})+`, 'g')
-      return v.toString().split(regex)
+      return v?.toString()?.split(regex) || v
     },
     optionFields: {
       delimiter: {
