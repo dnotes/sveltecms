e60fdaad168adecf2c166a1ee0e3d88ac82cf7cd
David Hunt <git@dnotes.net>
2021-11-21 10:49:36 -0800

    fixed multiple item delete button

--- src/lib/widgets/CMSWidgetMultiple.svelte
+++ src/lib/widgets/CMSWidgetMultiple.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+
 import type { SvelteCMSContentField } from "..";
 
   export let field:SvelteCMSContentField
@@ -9,11 +10,11 @@ import type { SvelteCMSContentField } from "..";
 
 </script>
 
-<fieldset class="cms-multiple">
+<fieldset class="cms-multiple" on:click|preventDefault>
   <label for="{id}[0]">{field.title}<label>
   {#each value as v,i}
   <div class="cms-multiple-item">
-    <button class="cms-multiple-item-delete" aria-label="Remove {field.title} item" on:click={() => { value = value.splice(i,1); value=value; }}>✖️</button>
+    <button class="cms-multiple-item-delete" aria-label="Remove {field.title} item" on:click={(e) => { debugger; value.splice(i,1); value=value; }}>✖️</button>
     <svelte:component
       this={field.widget.widget}
       bind:value={v}
