6b49d0bb438861e7df5d412efc644adeab3cd8c0
David Hunt <git@dnotes.net>
2022-05-05 13:47:57 -0700

    added very basic navigation

--- src/lib/CMSAdmin.svelte
+++ src/lib/CMSAdmin.svelte
@@ -8,19 +8,38 @@ import getLabelFromID from "./utils/getLabelFromID";
   export let adminPath:string
   export let data:Object = undefined
 
-  let basePath = $page.url.pathname.replace('/' + adminPath, '')
-
   let sections = Object.values(cms.adminPages)
     .filter(o => !o.id.match('/'))
 
+  $: basePath = $page.url.pathname.replace('/' + adminPath, '')
   $: adminPage = cms.getAdminPage(adminPath)
   $: title = adminPage ? adminPage.title ?? getLabelFromID(adminPath.replace(/\/.+/, '')) : 'Site Admin'
+  $: crumbs = adminPath.split('/').reduce((agg,val,i,arr) => {
+    if (i<arr.length-1) {
+      let base = agg.length ? agg[agg.length-1][1] : ''
+      agg.push([val, `${base}/${val}`])
+    }
+    return agg
+  }, [['admin', '']])
 
 </script>
 
 <svelte:head>
   <title>{title}</title>
 </svelte:head>
+
+<nav>
+  {#each sections as section}
+    <a href="{basePath}/{section.id}">{section.title || getLabelFromID(section.id)}</a>
+  {/each}
+</nav>
+<div class="breadcrumbs">
+  &nbsp;
+  {#each crumbs as [name, path], i}
+    <a href="{basePath}{path}"> &ltri; {name}</a>
+  {/each}
+</div>
+
 
 <h1>{title}</h1>
 
@@ -37,3 +56,11 @@ import getLabelFromID from "./utils/getLabelFromID";
     {/each}
   </ul>
 {/if}
+
+<style>
+  h1 { margin:10px 0; }
+  nav { position:fixed; top:0; left:0; padding:4px 0; width:100%; background:lightgray; }
+  nav>a { padding:2px 10px; display:inline-block; text-align:center; vertical-align:middle; }
+  .breadcrumbs { margin-top:2em; }
+  .breadcrumbs a { font-size:12px; font-weight:normal; display:inline-block; padding-left: 8px; vertical-align:middle; text-decoration:none; }
+</style>
