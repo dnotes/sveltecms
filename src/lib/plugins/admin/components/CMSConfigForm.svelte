dff20b533397576642f8736b6ca629ad3121447d
David Hunt <git@dnotes.net>
2022-05-17 11:14:42 -0700

    added exported 'data' prop to CMSConfigForm
    - gives ability to use AdminPage's get() prop to pass configPath

--- src/lib/plugins/admin/components/CMSConfigForm.svelte
+++ src/lib/plugins/admin/components/CMSConfigForm.svelte
@@ -3,6 +3,7 @@ import type SvelteCMS from "sveltecms";
 import { get, set, isEqual } from 'lodash-es'
 
   export let cms:SvelteCMS
+  export let data:string
   export let adminPath:string
   export let options:{
     component:string
@@ -10,17 +11,15 @@ import { get, set, isEqual } from 'lodash-es'
   }
   let opts = Object.assign({}, options)
   let component = cms.getEntity('components', opts.component)
-  let configPath = options.configPath || adminPath.replace(/\/.+/,'')
-  let data = cms.conf[configPath]
+  let configPath = data ?? options.configPath ?? adminPath.replace(/\//,'.')
+  let conf = get(cms.conf, configPath, {})
 
   // Variables for diffing
-  $: oldConf = get(cms.conf, options.configPath, {})
-  $: unsaved = !isEqual(oldConf, data) || !isEqual(Object.keys(oldConf), Object.keys(data))
-
-  $: console.log(data)
+  $: oldConf = get(cms.conf, configPath, {})
+  $: unsaved = !isEqual(oldConf, conf) || !isEqual(Object.keys(oldConf), Object.keys(conf))
 
   export let saveConfig = async () => {
-    set(cms.conf, configPath, data)
+    set(cms.conf, configPath, conf)
     let res = await fetch('/admin/config', {
       method: 'POST',
       headers: {
@@ -34,6 +33,6 @@ import { get, set, isEqual } from 'lodash-es'
 </script>
 
 <form method="dialog" on:submit|preventDefault={saveConfig}>
-  <svelte:component {cms} bind:data this={component.component} options={component.options}/>
+  <svelte:component {cms} bind:data={conf} this={component.component} options={component.options}/>
   <button type="submit" disabled={!unsaved}>Save</button>
 </form>
