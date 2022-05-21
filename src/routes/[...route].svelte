0b53ae67b93c5c352fa097f40524be5047c43b04
David Hunt <git@dnotes.net>
2022-05-21 13:15:46 -0700

    start on basic router / display

---
+++ src/routes/[...route].svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let content
+</script>
+
+{#await content}
+  fetching content...
+{:then content}
+  {JSON.stringify(content)}
+{:catch error}
+  {JSON.stringify(error)}
+{/await}
---
+++ src/routes/[...route].ts
@@ -0,0 +1,30 @@
+import cms from '$lib/cms'
+
+export async function get(event) {
+
+  const { params } = event
+  const [ contentTypeID, slug ] = params.route.split('/')
+
+  let content
+  try {
+    if (slug) {
+      content = await cms.getContent(contentTypeID, slug)
+      if (!content) return { status:404 }
+    }
+    else if (contentTypeID) {
+      content = await cms.listContent(contentTypeID)
+      if (!content) return { status: 404 }
+    }
+  }
+  catch(e) {
+    return {
+      status: 500,
+      body: JSON.stringify(e)
+    }
+  }
+  return {
+    status: 200,
+    body: { content }
+  }
+
+}
