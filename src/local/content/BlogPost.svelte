82268eeecc9233fabd7072f5e8060abf6aea946e
David Hunt <git@dnotes.net>
2022-05-25 06:44:49 -0700

    Fixed errors on new BlogPost

--- src/local/content/BlogPost.svelte
+++ src/local/content/BlogPost.svelte
@@ -5,22 +5,22 @@
 
   let lang = 'en-US'
   $: if (browser && window?.navigator?.language) lang = window.navigator.language
-  $: displayDate = item.date.toLocaleString(lang, { timeZone: 'America/Los_Angeles', timeZoneName: 'short' }).replace(/:\d{2} /, ' ')
+  $: displayDate = item?.date?.toLocaleString(lang, { timeZone: 'America/Los_Angeles', timeZoneName: 'short' }).replace(/:\d{2} /, ' ') || 'no date'
 
 </script>
 
 <svelte:head>
-  <title>{item.title} | SvelteCM Blog</title>
+  <title>{item.title || 'No Title'} | SvelteCMS Blog</title>
 </svelte:head>
 
-<h1>{item.title}</h1>
+<h1>{item.title || 'No Title'}</h1>
 <p class="date">{displayDate}</p>
 
 {#if item?.image?.src}
   <img src={item.image.src} alt={item.image?.alt || ''} />
 {/if}
 
-{@html item.body}
+{@html item?.body || ''}
 
 <style>
   img { width: 600px; max-width: 100%; }
