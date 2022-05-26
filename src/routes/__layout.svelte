70217775a74909bfc6e5af59a321ab4fbb429c8f
David Hunt <git@dnotes.net>
2022-05-25 18:28:53 -0700

    basic layout / style
    - added site nav to top
    - added index page
    - changed fonts to Roboto

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,26 @@
-<div class="sveltecms">
-  <slot></slot>
+<script>
+import Button from "sveltecms/ui/Button.svelte";
+</script>
+
+<div id="page" class="sveltecms">
+  <header>
+    <nav>
+      <ul>
+        <li><a href="/blog">Blog</a></li>
+        <li><a href="/admin">Admin</a></li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <slot></slot>
+  </main>
+  <footer>
+  </footer>
 </div>
 
-<style global>
+<style>
   @import "sveltecms/sveltecms-forms.css";
+  #page { display:flex; flex-direction:column; min-height:100vh; width:100vw; }
+  main { flex-grow:1; }
+  ul li { display: inline-block; padding: .3em .7em; }
 </style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Button from 'sveltecms/ui/Button.svelte'
+</script>
+
+<h1>Welcome to SvelteCMS!</h1>
+
+<p>We're just getting up and running, so this space is mostly empty.</p>
+
+<Button primary href="/admin">Check out the admin UI</Button>
