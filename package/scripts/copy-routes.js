b0d8b0dcd7ab368b428efebb2fb367049020d724
David Hunt <git@dnotes.net>
2022-06-21 00:49:18 -0700

    hopefully fixed paths in postinstall

--- package/scripts/copy-routes.js
+++ package/scripts/copy-routes.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 import cp from 'cp-file';
-const path = process.env.INIT_CWD;
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const fromPath = path.resolve(__dirname, '../routes');
+const toPath = process.env.INIT_CWD;
 const files = [
-    ['../routes/[...path].svelte', `${path}/routes/[...path].svelte`],
-    ['../routes/[...path].ts', `${path}/routes/[...path].ts`],
-    ['../routes/admin/[...adminPath].svelte', `${path}/routes/admin/[...adminPath].svelte`],
-    ['../routes/admin/[...adminPath].ts', `${path}/routes/admin/[...adminPath].ts`],
-    ['../routes/admin/__layout.svelte', `${path}/routes/admin/__layout.svelte`],
+    [`${fromPath}/[...path].svelte`, `${toPath}/routes/[...path].svelte`],
+    [`${fromPath}/[...path].ts`, `${toPath}/routes/[...path].ts`],
+    [`${fromPath}/admin/[...adminPath].svelte`, `${toPath}/routes/admin/[...adminPath].svelte`],
+    [`${fromPath}/admin/[...adminPath].ts`, `${toPath}/routes/admin/[...adminPath].ts`],
+    [`${fromPath}/admin/__layout.svelte`, `${toPath}/routes/admin/__layout.svelte`],
 ];
 files.forEach(([file, dest]) => {
     try {
