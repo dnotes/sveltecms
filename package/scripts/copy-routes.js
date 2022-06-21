729476873e41db3b8fa597ba4d67ca3d9f7cda97
David Hunt <git@dnotes.net>
2022-06-21 00:53:30 -0700

    ok one more fix should work

--- package/scripts/copy-routes.js
+++ package/scripts/copy-routes.js
@@ -4,13 +4,13 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const fromPath = path.resolve(__dirname, '../routes');
-const toPath = process.env.INIT_CWD;
+const toPath = path.resolve(process.env.INIT_CWD, 'src/routes');
 const files = [
-    [`${fromPath}/[...path].svelte`, `${toPath}/routes/[...path].svelte`],
-    [`${fromPath}/[...path].ts`, `${toPath}/routes/[...path].ts`],
-    [`${fromPath}/admin/[...adminPath].svelte`, `${toPath}/routes/admin/[...adminPath].svelte`],
-    [`${fromPath}/admin/[...adminPath].ts`, `${toPath}/routes/admin/[...adminPath].ts`],
-    [`${fromPath}/admin/__layout.svelte`, `${toPath}/routes/admin/__layout.svelte`],
+    [`${fromPath}/[...path].svelte`, `${toPath}/[...path].svelte`],
+    [`${fromPath}/[...path].ts`, `${toPath}/[...path].ts`],
+    [`${fromPath}/admin/[...adminPath].svelte`, `${toPath}/admin/[...adminPath].svelte`],
+    [`${fromPath}/admin/[...adminPath].ts`, `${toPath}/admin/[...adminPath].ts`],
+    [`${fromPath}/admin/__layout.svelte`, `${toPath}/admin/__layout.svelte`],
 ];
 files.forEach(([file, dest]) => {
     try {
