02e18acc8a868b137e1740fff88d5df52a2ead10
David Hunt <git@dnotes.net>
2022-06-29 11:31:18 -0700

    added tailwindcss for the site
    - Tailwind is the most popular utility CSS library
    - SvelteCMS will use it for the sveltecms.org site
    - SvelteCMS will NOT use Tailwind for any CMS components

--- svelte.config.js
+++ svelte.config.js
@@ -5,9 +5,11 @@ import yaml from '@rollup/plugin-yaml'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
 	adapter: adapter(),
 	kit: {
 		vite: {
