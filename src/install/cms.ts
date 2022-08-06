e602ead397a3af24767b0b39dcd2b7feff718522
David Hunt <git@dnotes.net>
2022-08-06 05:49:20 -0700

    trying to ship some sensible defaults
    - Fields: title, body, postDate, slug, mainImage, tags, taggedContent
    - Content Types: page, blog
    - Media Stores: images

--- src/install/cms.ts
+++ src/install/cms.ts
@@ -1,6 +1,9 @@
 import CMS from 'sveltecms'
 import conf from './sveltecms.config.json'
+import markdownPlugin from 'sveltecms/plugins/markdown'
 
-const cms = new CMS(conf)
+const cms = new CMS(conf, [
+  markdownPlugin({})
+])
 
 export default cms
