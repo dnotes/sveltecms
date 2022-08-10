134fb92a58a4d47b8ad35369960113f2a0549af0
David Hunt <git@dnotes.net>
2022-08-10 11:58:52 -0700

    added instructions for Tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,24 @@
-const extract = (content) => { return content.match(/(?<=\.)[-\w:]+/g) || [] }
+/**
+ * Function to extract classes from the text of matching content files.
+ * The regex-based search matches class names preceeded by a period.
+ * Class names are alpha-numerica, but may include dashes, underscores,
+ * or colons.
+ */
+const extract = (content) => {
+  return content.match(/(?<=\.)[-\w:]+/g) || []
+}
 
 const config = {
+  // The content array must be expanded into an object.
   content: {
+    // Files in this list should be checked for tailwind classes.
     files: [
       "./src/**/*.{html,js,svelte,ts}",
       "./src/sveltecms.config.{json,yml}",
       "./content/**/*.{md,yml,json}",
     ],
+    // The content.extract configuration tells Tailwind to use
+    // the function defined above for .json, .yml, and .md files.
     extract: {
       json: extract,
       yml: extract,
