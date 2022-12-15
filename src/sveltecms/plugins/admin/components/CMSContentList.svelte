9a6b5594f831da9fd3fb30ba19226272c6d87ded
David Hunt <git@dnotes.net>
2022-12-15 19:12:59 +0000

    use @muonw/powertable for content listings

--- src/sveltecms/plugins/admin/components/CMSContentList.svelte
+++ src/sveltecms/plugins/admin/components/CMSContentList.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-import { isArray } from "lodash-es";
-import type SvelteCMS from "sveltecms";
 import Button from "sveltecms/ui/Button.svelte";
+import { PowerTable } from '@muonw/powertable'
 
   export let adminPath:string
   export let basePath:string
@@ -11,12 +10,6 @@ import Button from "sveltecms/ui/Button.svelte";
 
 <Button primary href="{basePath}/{adminPath}/_">+ add</Button>
 
-<ul>
-  {#if Array.isArray(data)}
-    {#each (data || []) as item}
-      <li>{item._slug} <a href="{basePath}/{adminPath}/{item._slug}">[edit]</a></li>
-    {:else}
-      No items found.
-    {/each}
-  {/if}
-</ul>
+{#if Array.isArray(data)}
+  <PowerTable ptData={data} />
+{/if}
