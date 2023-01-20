c7d4d2e2063b6301b99a68a1efab6d3edbd8d836
David Hunt <git@dnotes.net>
2023-01-20 11:58:29 -0800

    removed PowerTable dep for breaking StackBlitz

--- src/sveltecms/plugins/admin/components/CMSContentList.svelte
+++ src/sveltecms/plugins/admin/components/CMSContentList.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
+import type SvelteCMS from "sveltecms";
+
 import Button from "sveltecms/ui/Button.svelte";
-import { PowerTable } from '@muonw/powertable';
+import PowerTable from 'sveltecms/ui/PowerTable.svelte';
 import { goto } from "$app/navigation";
 
   export let adminPath:string
   export let basePath:string
   export let data = []
+  export let cms:SvelteCMS
+
+  let [contentPath, contentTypeID, slug] = adminPath.split('/');
+
+  let contentType = cms.getEntity('contentType', contentTypeID)
 
   const ptOptions = {
     footerText:false,
@@ -18,26 +25,9 @@ import { goto } from "$app/navigation";
 
 {#if Array.isArray(data)}
   <div>
-    <PowerTable {ptOptions} ptData={data} on:rowClicked={(e) => goto(`/admin/content/${e.detail.data._type}/${e.detail.data._slug}`)} />
+    <PowerTable {cms} entity={contentType} {ptOptions} ptData={data} on:rowClicked={(e) => goto(`/admin/content/${e.detail.data._type}/${e.detail.data._slug}`)} />
   </div>
 {/if}
 
 <style>
-  div :global(td) {
-    cursor: pointer;
-  }
-  div :global(button) {
-    border: none;
-  }
-  div :global(th[data-dir="asc"]:after) {
-    content: "🔼";
-    font-weight: normal;
-    opacity: .6;
-  }
-  div :global(th[data-dir="desc"]:after) {
-    content: "🔽";
-    font-weight: normal;
-    opacity: .6;
-  }
-  div :global([data-key^="_"]) { display:none; }
 </style>
