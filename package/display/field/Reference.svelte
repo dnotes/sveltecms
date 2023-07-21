5f2a8d0cdd2d5ef560f78d3b5b53399de48a7d03
David Hunt <git@dnotes.net>
2023-07-21 09:22:54 -0700

    fixed breakage when displaying empty reference

--- package/display/field/Reference.svelte
+++ package/display/field/Reference.svelte
@@ -1,8 +1,8 @@
 <script>import ContentItem from '../ContentItem.svelte';
-export let cms;
-export let entity;
-export let item;
-$: contentType = cms.contentTypes[item._type] || { ...cms.defaultContentType, id: item._type };
-</script>
+  export let cms;
+  export let entity;
+  export let item;
+  $: contentType = cms.contentTypes?.[item?._type] || { ...cms.defaultContentType, id: item?._type };
+  </script>
 
-<ContentItem {cms} entity={contentType} {item} displayMode="{entity?.widget?.options?.displayMode?.toString() || 'reference'}" />
+  {#if item && contentType}<ContentItem {cms} entity={contentType} {item} displayMode="{entity?.widget?.options?.displayMode?.toString() || 'reference'}" />{/if}
